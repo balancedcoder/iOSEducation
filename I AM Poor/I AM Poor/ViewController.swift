af7c9d580cfa364c63f438b20b26c55a8bc617b5
balancedcoder <thebalancedcoder@gmail.com>
2018-06-08 04:06:22 -0700

    Code Challenge #1 Complete, building a version of I Am Rich alone

---
+++ I AM Poor/I AM Poor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I AM Poor
+//
+//  Created by BelieveInYourself on 6/8/18.
+//  Copyright © 2018 BelieveInYourself. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
